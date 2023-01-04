8d000a3d623112dfa9405b114eeb8145ba0c190c
Vadim <vadim.bertrand@lilo.org>
2023-01-04 09:08:09 +0100

    fix verbosity

--- run_grid_search.py
+++ run_grid_search.py
@@ -133,7 +133,7 @@ def build_hp_grid(pipe, seed, n_jobs, stab_sel_path):
     clf_grid = _create_grid(clf_grid_roots, clf_grid_params)
 
     final_grid = _merge_grids([sel_ind_grid, dim_red_grid, clf_grid])
-    cv_grid = GridSearchCV(pipe, final_grid, scoring="balanced_accuracy", n_jobs=n_jobs, verbose=1)
+    cv_grid = GridSearchCV(pipe, final_grid, scoring="balanced_accuracy", n_jobs=n_jobs, verbose=2)
 
     return cv_grid
 
